fcea1be522d20377c208528362f862eccac0a4ff
vram.vardanian <vram.vardanian@outlook.com>
2015-11-21 20:16:22 +0300

    merge sort implemented

--- week3/src/main.cpp
+++ week3/src/main.cpp
@@ -14,17 +14,34 @@ std::chrono::milliseconds testSort(unsigned size) {
     }
 
     std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-    my::insertion_sort(arr.begin(), arr.end());
+    // my::insertion_sort(arr.begin(), arr.end());
+    my::merge_sort_recursive(arr.begin(), arr.end());
     std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
 
     return std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
 }
 
+template<typename T>
+void printVector(const std::vector<T>& vec, const char* prefix) {
+    std::cout << prefix;
+    for(auto a : vec) 
+        std::cout << a << ", ";
+    std::cout << std::endl;
+}
+
+template <typename RandomAccessIterator>
+void printRange(const char* prefix, RandomAccessIterator first, RandomAccessIterator last) {
+    std::cout << prefix;
+    while(first != last)
+        std::cout << *(first++) << ", ";
+    std::cout << std::endl;
+}
 
 int main() {
+
     std::srand(std::time(0));
 
-    std::vector<unsigned> sizes = {10000, 20000, 40000, 80000, 160000, 320000};
+    std::vector<unsigned> sizes = {100000, 200000, 400000};
         for(auto sizeIt = sizes.begin(); sizeIt != sizes.end(); sizeIt++) {
         auto durationMs = testSort(*sizeIt);
         std::cout << "size =     " << *sizeIt << std::endl;
--- week3/src/sort.h
+++ week3/src/sort.h
@@ -1,8 +1,14 @@
+#include <vector>
 #include <iterator>
-
+#include <algorithm>
 
 namespace my {
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    // insertion sort algorithm:
+    // O(N^2) running time
+    // not stable 
+    // O(1) extra memory
     template <typename RandomAccessIterator>
     void insertion_sort(RandomAccessIterator first, RandomAccessIterator last) {
        if(first >= last)
@@ -12,7 +18,7 @@ namespace my {
            RandomAccessIterator lastSorted = current - 1;
            // determine position where 'current' element must be inserted
            RandomAccessIterator insertPos = current;
-           while(*(insertPos - 1) > *current)
+           while(*(insertPos - 1) > *current && insertPos > first)
                --insertPos;
            // make room for element to be inserted
            typename RandomAccessIterator::value_type tmpVal = *current;
@@ -24,12 +30,78 @@ namespace my {
        }
     }
 
-#if 0
-    template <typename RandomAccessIterator>
-    void merge_sort(RandomAccessIterator first, RandomAccessIterator last);
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    // merge sort algorithm (recursive):
+    // O(N * lgN) running time
+    // stable 
+    // O(N) extra memory
+
+    template <typename T>
+    using BufferType = std::vector<T>;
+
+    template <typename RandomAccessIterator, typename ValueType>
+    void merge(RandomAccessIterator first, RandomAccessIterator middle, RandomAccessIterator last, BufferType<ValueType>& mergeBuff) {
+        auto itLeft = first;
+        auto itRight = middle;
+        auto itMerged = mergeBuff.begin();
+        for(auto i = 0; i < last - first; ++i) {
+            if(itRight == last)
+                *itMerged = *(itLeft++);
+            else if(itLeft == middle)
+                *itMerged = *(itRight++);
+            else if(*itLeft <= *itRight)
+                *itMerged = *(itLeft++);
+            else 
+                *itMerged = *(itRight++);
+            ++itMerged;
+        }
+        std::copy(mergeBuff.begin(), itMerged, first);
+    }
+
+    template <typename RandomAccessIterator, typename ValueType>
+    void sort_helper(RandomAccessIterator first, RandomAccessIterator last, BufferType<ValueType>& mergeBuff) {
+        auto problemSize = std::distance(first, last);
+        if(problemSize < 7) {
+            insertion_sort(first, last);
+            return;
+        }
+        RandomAccessIterator middle = first + problemSize / 2;
+        sort_helper(first, middle, mergeBuff);
+        sort_helper(middle, last, mergeBuff);
+        merge(first, middle, last, mergeBuff);
+    }
 
     template <typename RandomAccessIterator>
-    void quick_sort(RandomAccessIterator first, RandomAccessIterator last);
+    void merge_sort_recursive(RandomAccessIterator first, RandomAccessIterator last) {
+        if(first >= last)
+            return;
+        auto buffSize = last - first;
+        // buffer we use to merge to sorted arrays
+        BufferType<typename RandomAccessIterator::value_type> mergeBuff(buffSize);
+        sort_helper(first, last, mergeBuff);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    // quick sort algorithm
+    // O(N * lgN) average running time, O(N^2) worst case
+    // not stable 
+    // O(1) extra memory
+    
+    template <typename RandomAccessIterator>
+    void quick_sort(RandomAccessIterator first, RandomAccessIterator last) {
+
+
+
+    }
+
+
+#if 0
+
+    template <typename RandomAccessIterator>
+    void merge_sort_bottom_up(RandomAccessIterator first, RandomAccessIterator last) {
+
+    }
+
 
     template <typename RandomAccessIterator>
     void quick_sort_3w(RandomAccessIterator first, RandomAccessIterator last);
